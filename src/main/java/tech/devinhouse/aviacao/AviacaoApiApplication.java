5ade501eb1b18af068872714e86bc89c49a299b7
DESKTOP-7N279EE\Leandro <leo1996.dasilva@gmail.com>
2023-05-31 21:58:47 -0300

    First commit.

---
+++ src/main/java/tech/devinhouse/aviacao/AviacaoApiApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.aviacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/devinhouse/aviacao/AviacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.aviacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
