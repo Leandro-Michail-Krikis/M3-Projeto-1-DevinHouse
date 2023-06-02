4afa15dbe25d9b2a659fff8b6a29d50c00a827b5
DESKTOP-7N279EE\Leandro <leo1996.dasilva@gmail.com>
2023-06-01 21:45:10 -0300

    Criação do repository do passageiro

---
+++ src/test/java/tech/devinhouse/aviacao/repositories/PassageiroRepository.java
@@ -0,0 +1,7 @@
+package tech.devinhouse.aviacao.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PassageiroRepository extends CrudRepository<PassageiroRepository, String>{
+
+}
