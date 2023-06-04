73592c50dfa1dbc024be0ccf85e7cc1222f3a5a3
DESKTOP-7N279EE\Leandro <leo1996.dasilva@gmail.com>
2023-06-04 18:08:34 -0300

    Criação do endpoint para retornar todos os passageiros

---
+++ src/main/java/tech/devinhouse/aviacao/controllers/PassageirosController.java
@@ -0,0 +1,38 @@
+package tech.devinhouse.aviacao.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import tech.devinhouse.aviacao.controllers.dtos.request.PassageiroResponse;
+import tech.devinhouse.aviacao.controllers.mappers.PassageiroMapper;
+import tech.devinhouse.aviacao.service.EticketService;
+import tech.devinhouse.aviacao.service.PassageiroService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/passageiros")
+public class PassageirosController {
+
+	private final PassageiroService passageiroService;
+	
+	private final EticketService eticketService;
+	
+	private final PassageiroMapper passageiroMapper;
+	
+	@GetMapping
+	public ResponseEntity<List<PassageiroResponse>> getAll() {
+		List<PassageiroResponse> listaResponse = new ArrayList<>();
+		for (var passageiro : passageiroService.findAll()) {
+			var eticket = eticketService.findByPassageiro(passageiro);
+			listaResponse.add(passageiroMapper.map(passageiro, eticket));
+		}
+		return ResponseEntity.ok(listaResponse);
+	}
+	
+}
